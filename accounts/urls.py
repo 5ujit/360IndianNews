b75d503db945922714e453cc9ea84e53d5a906c4
5ujit <sksujit021@gmail.com>
2025-11-16 23:14:15 +0530

    chages

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'accounts'  # helps with namespacing URLs
 
 urlpatterns = [
-    # path('login/', views.login_view, name='login'),  # Example:  You'll need to create views
+    
     # path('signup/', views.signup_view, name='signup'),
     path('signup/', views.signup, name='signup'), # add this
 ]
